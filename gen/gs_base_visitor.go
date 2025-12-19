de1f388cf1ac1b68b9470d6bffaa8ebd33a98af0
ycl2018 <chenglongustb@gmail.com>
2025-12-19 21:26:57 +0800

    fix: gen

--- gen/gs_base_visitor.go
+++ gen/gs_base_visitor.go
@@ -1,4 +1,4 @@
-// Code generated from /Users/bytedance/go/src/github.com/ycl2018/gs/Gs.g4 by ANTLR 4.13.2. DO NOT EDIT.
+// Code generated from github.com/ycl2018/gs/Gs.g4 by ANTLR 4.13.2. DO NOT EDIT.
 
 package gen // Gs
 
